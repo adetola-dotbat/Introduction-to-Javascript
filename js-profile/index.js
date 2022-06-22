2aaedddc4aff12c4fad4b40db40a867fc61d1f92
adetola-dotbat <105286553+adetola-dotbat@users.noreply.github.com>
2022-06-22 16:47:16 +0100

    Add files via upload

---
+++ js-profile/index.js
@@ -0,0 +1,5 @@
+document.getElementById("name").innerHTML = "Adewole Noah"
+document.getElementById("height").innerHTML = "85.5"
+document.getElementById("location").innerHTML = "Ejigbo, Lagos State"
+
+// name = 
